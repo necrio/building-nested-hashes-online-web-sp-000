9ab9e69f8820d8495dc28a9e58bbc66642c8f39b
Keith Isham <kisham8899@gmail.com>
2019-05-03 18:10:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,10 +6,10 @@ def second_challenge
       :hero => {},
       :hero_friends => []
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
